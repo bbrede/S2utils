2790bb89bd4b848068c057564a8ce3633fb1689d
Brede <benjamin.brede@wur.nl>
2016-03-16 16:50:51 +0100

    remove laziness

--- R/S2_L2A_granule.R
+++ R/S2_L2A_granule.R
@@ -5,7 +5,6 @@
 #' @param granule_folder Folder that contains the S2 L2A granule
 #' @param band Band to extract, can be spectral (B02 to B12 plus B8A) or thematic (SCL or CLD)
 #' @param resolution Band resolution in m
-#' @param lazy If filename is given and target file already exists, return this as RasterLayer.
 #' @param ... Additional arguments as for \code{\link{writeRaster}}
 #' 
 #' @return RasterLayer
@@ -19,49 +18,40 @@
 #' @import rgdal
 
 
-S2_L2A_granule <- function(granule_folder, band, resolution=c(10, 20, 60), lazy=FALSE, ...) {
+S2_L2A_granule <- function(granule_folder, band, resolution=c(10, 20, 60)) {
   
   library(raster)
   library(gdalUtils)
   library(rgdal)
   
-  args <- list(...)
+  # extract corner coordinates, cell size, CRS from xml
+  geo_info <- S2_extract_geoinfo(list.files(granule_folder, 'S2A.+xml$', full.names = TRUE, recursive = FALSE), resolution)
   
-  if (lazy & 'filename' %in% names(args) & file.exists(filename)) {
-    
-    raster(filename)
-    
-  } else {
-    
-    # extract corner coordinates, cell size, CRS from xml
-    geo_info <- S2_extract_geoinfo(list.files(granule_folder, 'S2A.+xml$', full.names = TRUE, recursive = FALSE), resolution)
-    
-    jp2 <- list.files(path = granule_folder,
-                      pattern = paste0(band, '_.*', resolution, 'm.jp2'),
-                      full.names = TRUE, recursive = TRUE)
-    
-    r <- gdal_translate(src_dataset = jp2, dst_dataset = rasterTmpFile(), output_Raster = TRUE)
-    
-    # assign CRS and extent
-    crs(r) <- CRS(paste0('+init=', tolower(geo_info$EPSG)))
-    extent(r) <- extent(c(geo_info$UL_corner$ULX,
-                          geo_info$UL_corner$ULX + resolution * geo_info$dimensions$NCOLS,
-                          geo_info$UL_corner$ULY - resolution * geo_info$dimensions$NROWS,
-                          geo_info$UL_corner$ULY))      
-    
-    
-    # write raster if filename is supplied
-    if ('filename' %in% names(args))
-      writeRaster(r, ...)
-    else 
-      r
-    
-    ### GDALWARP - did not work
-    # target extent in UTM
-    #te <- c(geo_info$UL_corner$ULX, 
-    #        geo_info$UL_corner$ULY - resolution * geo_info$dimensions$NROWS,
-    #        geo_info$UL_corner$ULX + resolution * geo_info$dimensions$NCOLS,
-    #        geo_info$UL_corner$ULY)
-    #gdalwarp(jp2, dst, t_srs = geo_info$EPSG, te = te) 
-  }
+  jp2 <- list.files(path = granule_folder,
+                    pattern = paste0(band, '_.*', resolution, 'm.jp2'),
+                    full.names = TRUE, recursive = TRUE)
+  
+  r <- gdal_translate(src_dataset = jp2, dst_dataset = rasterTmpFile(), output_Raster = TRUE)
+  
+  # assign CRS and extent
+  crs(r) <- CRS(paste0('+init=', tolower(geo_info$EPSG)))
+  extent(r) <- extent(c(geo_info$UL_corner$ULX,
+                        geo_info$UL_corner$ULX + resolution * geo_info$dimensions$NCOLS,
+                        geo_info$UL_corner$ULY - resolution * geo_info$dimensions$NROWS,
+                        geo_info$UL_corner$ULY))      
+  
+  
+  # write raster if filename is supplied
+  if ('filename' %in% names(args))
+    writeRaster(r, ...)
+  else 
+    r
+  
+  ### GDALWARP - did not work
+  # target extent in UTM
+  #te <- c(geo_info$UL_corner$ULX, 
+  #        geo_info$UL_corner$ULY - resolution * geo_info$dimensions$NROWS,
+  #        geo_info$UL_corner$ULX + resolution * geo_info$dimensions$NCOLS,
+  #        geo_info$UL_corner$ULY)
+  #gdalwarp(jp2, dst, t_srs = geo_info$EPSG, te = te) 
 }

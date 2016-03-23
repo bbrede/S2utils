aaf74c201e7e73b4ded17bda5c11dcb554855e9b
Brede <benjamin.brede@wur.nl>
2016-03-23 18:35:01 +0100

    change S2_translate output from mosaic to list of RasterLayers

--- R/S2_L2A_translate.R
+++ R/S2_L2A_translate.R
@@ -2,16 +2,15 @@
 #' 
 #' Translate an S2 L2A scene into other raster format.
 #' 
-#' @description Single granules are mosaiced to form one output raster.
-#' 
 #' @param S2_folder Folder that contains the S2 L2A product (typically suffixed with .SAFE)
 #' @param band Band to extract, can be spectral (B01 to B12 plus B8A) or thematic (SCL or CLD)
 #' @param resolution Band resolution in m
-#' @param filename Output filename
+#' @param filename Output filename, single tiles will be suffixed with tileID
+#' @param ... Additional arguments as for \code{\link{writeRaster}}
 #' 
-#' @return RasterLayer corresponding to the mosaiced raster of the specified band and resolution
+#' @return List of RasterLayers corresponding to the raster of the specified band and resolution
 #' 
-#' @details rasterOptions apply for writing the return raster
+#' @details rasterOptions apply for writing the return rasters
 #'
 #' @export
 #' 
@@ -24,20 +23,13 @@
 # date: 2016-01-16
 
 
-S2_L2A_translate <- function(S2_folder, band, resolution=c(10, 20, 60), filename) {
-  
-  library(raster)
-  library(gdalUtils)
-  library(rgdal)
-    
+S2_L2A_translate <- function(S2_folder, band, resolution=c(10, 20, 60), filename, ...) {
+      
   # TODO: require gdal version >= 2.1 -> utils::compareVersion 
   
   # list all granule folders
   all_granules <- list.dirs(file.path(S2_folder, 'GRANULE'), full.names = TRUE, recursive = FALSE) 
   
   # extract the single tiles
-  rsts <- lapply(all_granules, S2_L2A_granule, band = band, resolution = resolution)
-  rst_names <- sapply(rsts, filename)
-  
-  mosaic_rasters(rst_names, dst_dataset = filename, output_Raster = TRUE)
+  lapply(all_granules, S2_L2A_granule, band = band, resolution = resolution, filename = filename, ...)
 }

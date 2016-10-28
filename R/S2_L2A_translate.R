68baacf8b18e105626aff723cab6db28c496d20d
Brede <benjamin.brede@wur.nl>
2016-10-28 11:01:04 +0200

    add documentation

--- R/S2_L2A_translate.R
+++ R/S2_L2A_translate.R
@@ -3,14 +3,14 @@
 #' Translate an S2 L2A scene into other raster format.
 #' 
 #' @param S2_safe Chr. S2 SAFE folder (S2A_USER_PRD_MSIL2A_PDMC_....SAFE)
-#' @param band Chr. Band to extract, see \code{\link{S2_bands}}
-#' @param resolution Num. Resolution in m, allowed: 10, 20, 60
+#' @param band Chr. Band to extract, see \code{\link{S2_bands}}.
+#' @param resolution Num. Resolution in m, allowed: 10, 20, 60.
 #' @param granules Chr. Granules to translate, prefixes with 'T' (e.g. 'T32ULD'), 'all' for all granules in the product (see \link{https://sentinel.esa.int/documents/247904/1955685/S2A_OPER_GIP_TILPAR_MPC__20151209T095117_V20150622T000000_21000101T000000_B00.kml})
 #' @param filename Chr. Output filename; will be automatically suffixed with granuleID, band name and resolution
 #' @param sep Chr. Seperator for suffixed filenames
 #' @param ... Additional arguments as for \code{\link{gdal_translate}}, not allowed: a_srs, a_ullr.
 #' 
-#' @return List of RasterLayers corresponding to the raster of the specified band and resolution
+#' @return Named list of Chr[], containing the filenames. Names are granule identifiers.
 #'
 #' @author Benjamin Brede
 #' 
@@ -25,7 +25,7 @@
 #' 
 #' S2 <- 'S2A_USER_PRD_MSIL2A_PDMC_20151001T124224_R008_V20151001T104705_20151001T104705.SAFE'
 #' # use co argument to access creation options for GTiff files as in gdal_translate
-#' S2_L2A_translate(S2, 'B8A', 20, 'test.tif', co = c('COMPRESS=LZW'))  
+#' S2_L2A_translate(S2, band = 'B8A', resolution = 20, granules = 'T31UFU', 'test.tif', co = c('COMPRESS=LZW'))  
 
 S2_L2A_translate <- function(S2_safe, band, resolution = c(10, 20, 60), granules = 'all', filename, sep = '_', ...) {
       
@@ -41,7 +41,7 @@ S2_L2A_translate <- function(S2_safe, band, resolution = c(10, 20, 60), granules
   }
   
   # process per granule
-  lapply(proc_granules, function(g) {
+  out <- lapply(proc_granules, function(g) {
     
     granule_meta <- s2_meta[[g]]
     # granule folder
@@ -86,4 +86,8 @@ S2_L2A_translate <- function(S2_safe, band, resolution = c(10, 20, 60), granules
     
     unlist(out_list)
   })
+  
+  # assign granule names to list elements
+  names(out) <- proc_granules
+  out
 }
